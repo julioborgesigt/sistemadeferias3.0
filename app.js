abbcb5f242c5753b44988a082d022497931363e8
julioborgesigt <julio.aparecido3@gmail.com>
2025-10-09 18:24:07 -0300

    reformulado

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-      secure: false, //process.env.NODE_ENV === 'production',
+      secure: false, // para o ambiente de teste pode deixar process.env.NODE_ENV === 'production',
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000
   }
